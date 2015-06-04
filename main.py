7921a1fef57ff93c62cca304ffe7bc5c32111651
rjannluyc <jannluy@gmail.com>
2015-06-04 13:00:43 -0600

    Barrido Ping

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-print "Welcome 1"
+import subprocess
+import os
+import socket
+import platform
+import sys
+x=0
+print ""
+var_network = raw_input("Segmento de RED: ")
+var_ipinit = input("IP inicial: ")
+var_ipend = input("IP final: ")
+print ""
+with open(os.devnull, "wb") as limbo:
 
-print "Welcome 2"
+
+        for n in xrange(var_ipinit, var_ipend):
+            try:
+                ip= var_network+`n`
+                result=subprocess.Popen(["ping", "-n", "1", "-w", "1", ip],
+                        stdout=limbo, stderr=limbo).wait()
+                if result:
+                        print ip, "Inactive"
+                else:
+                        print ip, "Active", socket.gethostbyaddr(ip)
+                        x=x+1
+
+            except socket.error, msg:
+                    print msg
+print ""
+print "Nodos Activos: ", x
