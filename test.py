788bbed92543f5a3d47b0886f6e18de426fe5b35
framos <franciscohoracio@gmail.com>
2015-06-06 11:56:04 -0500

    IP Calc format X.X.X.X/X or X.X.X.X X.X.X.X

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 class IP_Calc(object):
+
     res = []
 
     def __init__(self,cadena):
-        self.ip_str = cadena.split("/")[0]
-        self.ip_bin = ''.join([bin(int(x)+256)[3:] for x in self.ip_str.split('.')])
-        self.netmask_cidr = cadena.split("/")[1]
+        if len(cadena.split("/")) == 2:
+            self.netmask_cidr = cadena.split("/")[1]
+            self.ip_str = cadena.split("/")[0]
+        else:
+            self.netmask_cidr = str(self.__ip_to_bin(cadena.split(" ")[1]).find("0"))
+            self.ip_str = cadena.split(" ")[0]
+        self.ip_bin = self.__ip_to_bin(self.ip_str)
         self.netmask_str = ""
         self.broadcast_str = ""
         self.network_str = ""
@@ -17,6 +22,10 @@ class IP_Calc(object):
 
     def __bin_to_int(self,bin_str):
         return int(bin_str, base=2)
+
+    def __ip_to_bin(self,ip):
+        return ''.join([bin(int(x)+256)[3:] for x in ip.split('.')])
+        self.res = self.__calc()
 
     def __calc(self):
         broadcast_bin = ""
@@ -45,7 +54,7 @@ class IP_Calc(object):
                 self.res.append(self.__bin_to_ip(str(bin(key))[2:]))
             return self.res
 
-ips = IP_Calc("192.168.1.0/24")
+ips = IP_Calc("192.168.1.34 255.255.255.0")
 print "\n\nIP", ips.ip_str
 print "Netmask", ips.netmask_str
 print "Network", ips.network_str
@@ -53,7 +62,13 @@ print "Ip Minima", ips.ip_min_str
 print "Ip Maxima", ips.ip_max_str
 print "Broadcast", ips.broadcast_str
 print "Cantidad de Host", ips.quantity_host
-print "Ips"
-for key in ips.res:
-    print key
 
+
+ips = IP_Calc("192.168.1.34/24")
+print "\n\nIP", ips.ip_str
+print "Netmask", ips.netmask_str
+print "Network", ips.network_str
+print "Ip Minima", ips.ip_min_str
+print "Ip Maxima", ips.ip_max_str
+print "Broadcast", ips.broadcast_str
+print "Cantidad de Host", ips.quantity_host
